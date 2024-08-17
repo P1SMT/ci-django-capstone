a04f4bf1d1bf5498c7dd0f1b77386fa2570671d5
Steve Taylor <steve@p1smt.com>
2024-08-17 20:04:54 +0000

    add 'weather' django app using startapp command

--- smt_capstone/settings.py
+++ smt_capstone/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary_storage',
-    'cloudinary'
+    'cloudinary',
+    'weather'
 ]
 
 MIDDLEWARE = [
