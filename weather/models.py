7c44549adc25f75dc29c493e5e0284b962fc6fd1
Steve Taylor <steve@p1smt.com>
2024-08-30 15:39:28 +0000

    Added City model to handle user favorite cities

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} - {self.user.username}'
