2577568630628944dc9b6511915b282758df5b2d
Steve Taylor <steve@p1smt.com>
2024-08-30 17:24:39 +0000

    fix errors with database migration so that index.html page now displays

---
+++ weather/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import City
+
+class CityForm(forms.ModelForm):
+    class Meta:
+        model = City
+        fields = ['name']
+
+class UserRegisterForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+
+        return cleaned_data
